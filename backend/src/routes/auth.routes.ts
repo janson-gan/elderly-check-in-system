3e5f998aedca718d5a1d8bd03acc501d3b15f69d
Janson-Gan <janson.gbc@gmail.comgit config --global user.name>
2026-03-25 20:22:18 +0800

    feat: Add new user and family-senior routes

--- backend/src/routes/auth.routes.ts
+++ backend/src/routes/auth.routes.ts
@@ -3,22 +3,48 @@ import { register, login, getUser } from "../controllers/auth.controller";
 import validate from "../middleware/validate";
 import { registerSchema, loginSchema } from "../types/validation";
 import authenticate from "../middleware/authenticate";
+import authorise from "../middleware/authorise";
 
 const router = Router();
 
-// @route    Post /api/auth/register
+// @route    POST /api/auth/register
 // @desc     Register a new user
 // @access   Public
 router.post("/register", validate(registerSchema), register);
 
-// @route    Post /api/auth/login
+// @route    POST /api/auth/login
 // @desc     Login user and return token
 // @access   Public
 router.post("/login", validate(loginSchema), login);
 
-// @route    Get /api/auth/user
+// @route    GET /api/auth/user
 // @desc     Get current login user
 // @access   Private
 router.get("/user", authenticate, getUser);
+
+// @route    GET /api/auth/admin-only
+// @desc     Test admin only route
+// @access   Admin only
+router.get("/admin-only", authenticate, authorise("admin"), (req, res) => {
+  res.json({
+    success: true,
+    message: `Welcome Admin ${req.user?.email}`,
+  });
+});
+
+// @route    GET /api/auth/family-senior
+// @desc     Test family and senior route
+// @access   Family and senior
+router.get(
+  "/family-senior",
+  authenticate,
+  authorise("family", "senior"),
+  (req, res) => {
+    res.json({
+      success: true,
+      message: `Welcome ${req.user?.role} - ${req.user?.email}`,
+    });
+  },
+);
 
 export default router;
